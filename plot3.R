409a05e4fb553c1269503d46da8a17acc62b047d
Shubhamr06 <32364476+Shubhamr06@users.noreply.github.com>
2018-02-05 13:36:41 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,6 @@
+with(df, plot(FullTimeDate, Sub_metering_1, type="l", xlab="Day", ylab="Energy sub metering"))
+lines(df$FullTimeDate, df$Sub_metering_2,type="l", col= "red")
+lines(df$FullTimeDate, df$Sub_metering_3,type="l", col= "blue")
+legend(c("topright"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty= 1, lwd=2, col = c("black", "red", "blue"))
+dev.copy(png,"plot3.png",height=480,width=480)
+dev.off()
